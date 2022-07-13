fbece0dfcf1466fcc504bb12717aeeb69a790d2c
Deckstop <5621kvs@gmail.com>
2022-07-13 23:03:55 +0300

    d

---
+++ FoodApi/Controllers/ListData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListData : ControllerBase
+    {
+        [HttpGet()]
+        public ContentResult Get()
+        {
+
+            return base.Content("",
+           "application/json; charset=utf-8");
+        }
+    }
+}
