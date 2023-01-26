d8b7898043c514021b68ea4fd1f496dbe43aed5b
sava5621 <88454060+sava5621@users.noreply.github.com>
2023-01-26 18:49:23 +0300

    Update Program.cs

--- FoodApi/Program.cs
+++ FoodApi/Program.cs
@@ -12,7 +12,7 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        var connectionString = "server=u1648633.plsk.regruhosting.ru;port=3306;user=u1648633_admin;password=Y79-VkR-iFi-RsX;database=u1648633_food_db;";
+        var connectionString = "//";
 
         builder.Services.AddDbContextPool<dbContext>(options => options
             .UseMySql(
